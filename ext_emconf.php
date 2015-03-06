a1e1620ecf272199913eb2b35489e86b3fd15a3a
root <root@46-typo3-62-dev.ip-5-39-66.eu>
2015-03-06 13:19:50 +0100

    Initial Commit

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pradinestemplate".
+ *
+ * Auto generated 23-08-2014 13:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Adico theme Decouverte',
+	'description' => 'Theme Decouverte ',
+	'category' => 'misc',
+	'version' => '0.0.1',
+	'state' => 'beta',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Yann Bogdanovic',
+	'author_email' => 'yann.bogdanovic@cdgfpt46.fr',
+	'author_company' => 'CDGFPT 46',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'bootstrap_package' => '6.2.9-6.99.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
